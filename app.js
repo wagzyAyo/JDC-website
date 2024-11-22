804e9ad245b38d00f9d1d5fedb18cfa6cffed1dc
Ayo <talktojmcvibes@gmail.com>
2024-11-22 11:40:45 +0100

    gallery section

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // alert(`Welcome to Jennifer Dede's Clothing`)
+document.addEventListener('DOMContentLoaded', function (){
+    const swiper = new Swiper('.swiper', {
+        slidePerView : 1,
+        spaceBetween: 20,
+     
+     
+       
+         // Navigation arrows
+         navigation: {
+           nextEl: '.swiper-button-next',
+           prevEl: '.swiper-button-prev',
+         },
+     
+         breakPoint: {
+             768: {
+                 slidePerView: 2,
+             },
+             1024: {
+                 slidePerView: 3,
+             }
+         }
+       
+       });
+})
