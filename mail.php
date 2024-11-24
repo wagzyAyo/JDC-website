afa7abb6584e7a754b910e2f379fefa43cd3ff2d
Ayo <talktojmcvibes@gmail.com>
2024-11-24 11:56:12 +0100

    success mail

---
+++ mail.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $number = $_POST["number"];
+    $age = $_POST["age"];
+    $gender = $_POST["gender"];
+    $location = $_POST["location"]
+
+    $from = "jmcvibes@gmail.com"; 
+    $to = "ajacctinglens@gmail.com";
+    $subject = "New message from $name";
+    $body = "Name: $name\nEmail: $email\n\nphone Number: $phone\n\nage: $age\n\n\n\ngender: $gender\n\nlocation: $location";
+
+    $headers = "From: $from \r\n"; 
+
+    $headers .= "Reply-To: $email \r\n"; 
+
+    if (mail($to, $subject, $body, $headers)) { 
+        header("Location: success.html");
+        exit; 
+    } else {
+        echo "Error: Unable to send message";
+    }
+}
+?>
