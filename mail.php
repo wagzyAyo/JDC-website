df27e26e4e0481df1413a16c7a3a07f67fd4ae21
Ayo <talktojmcvibes@gmail.com>
2024-12-09 13:01:46 +0100

    update mail.php

--- mail.php
+++ mail.php
@@ -1,26 +1,28 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $number = $_POST["number"];
     $age = $_POST["age"];
     $gender = $_POST["gender"];
-    $location = $_POST["location"]
+    $location = $_POST["location"];
+
 
     $from = "info@ajaccountinglens.com"; 
     $to = "talktojmcvibes@gmail.com";
     $subject = "New message from $name";
-    $body = "Name: $name\nEmail: $email\n\nphone Number: $phone\n\nage: $age\n\n\n\ngender: $gender\n\nlocation: $location";
+    $body = "Name: $name\nEmail: $email\nPhone Number: $number\nAge: $age\nGender: $gender\nLocation: $location";
 
-    $headers = "From: $from \r\n"; 
-
-    $headers .= "Reply-To: $email \r\n"; 
+    $headers = "From: $from \r\n";
+    $headers .= "Reply-To: $email \r\n";
 
     if (mail($to, $subject, $body, $headers)) { 
         header("Location: success.html");
-        exit; 
+        exit;
     } else {
         echo "Error: Unable to send message";
     }
+} else {
+    echo "Invalid request method.";
 }
 ?>
